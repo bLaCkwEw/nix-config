e8c8ad68a0d85ab2b5a965b6b4bf5c8a1b9fc3ad
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2023-07-23 16:54:43 +0300

    add configs

--- home.nix
+++ home.nix
@@ -11,6 +11,13 @@
   ];
 
   home.file = {
+  ".gitconfig".source = ./config/gitconfig;
+  ".config/alacritty/alacritty.yml".source = ./config/alacritty/alacritty.yml;
+  ".config/alacritty/catppuccin-mocha.yml".source = ./config/alacritty/catppuccin-mocha.yml;
+  ".config/mpv/mpv.conf".source = ./config/mpv/mpv.conf;
+  ".config/mpv/input.conf".source = ./config/mpv/input.conf;
+  ".config/neofetch/config.conf".source = ./config/neofetch/config.conf;
+  ".tmux.conf".source = ./config/tmux.conf;
   
   };
 
@@ -20,3 +27,4 @@
   
   programs.home-manager.enable = true;
 }
+
