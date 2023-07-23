b06ab19a9d8c34cd94b6856d824e69a0c1da74ec
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2023-07-23 16:23:38 +0300

    add git config

---
+++ home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "blackwew";
+  home.homeDirectory = "/home/blackwew";
+  
+  home.stateVersion = "22.11";
+  
+  home.packages = [
+    
+  ];
+
+  home.file = {
+  
+  };
+
+  home.sessionVariables = {
+    
+  };
+  
+  programs.home-manager.enable = true;
+}
