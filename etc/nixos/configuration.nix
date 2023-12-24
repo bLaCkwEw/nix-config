c9da3acea02fef5df0e6cf954067fbf887f3bfee
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2023-12-24 19:26:19 +0200

    remove obsidian as it prevents updates due to use of old electron version

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -5,6 +5,7 @@
     [
       ./hardware-configuration.nix
       ./vm.nix
+      ./other.nix
     ];
   
   
--- home.nix
+++ home.nix
@@ -12,7 +12,7 @@
   home.packages = with pkgs; [
   	#
     vscode
-    obsidian
+    #obsidian
     spotify
     discord
     #
