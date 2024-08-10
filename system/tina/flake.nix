f8f5e053355a601aea4d8c4c958e8d442cafaec4
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2024-08-10 13:14:32 +0300

    update system
    - fix an issue in tina config

--- system/tina/flake.nix
+++ system/tina/flake.nix
@@ -7,7 +7,7 @@
     let lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
-        nixos = lib.nixosSystem {
+        tina = lib.nixosSystem {
           system = "x86_64-linux";
           modules = [ ./configuration.nix ];
         };
