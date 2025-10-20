fe55f273c855cfc8ab5993fa8d2e4887e083ccb0
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2025-10-20 23:32:46 +0300

    update system 2025-10-20

--- modules/git.nix
+++ modules/git.nix
@@ -1,23 +1,22 @@
-{ ... }:
-
-{
+{...}: {
   # Git
   programs.git = {
-  	enable = true;
-  	
-  	userName = "bLaCkwEw";
-		userEmail = "35146970+bLaCkwEw@users.noreply.github.com";
-  	
-  	extraConfig = {
-	  	init.defaultBranch = "master";
-	  	credential.helper = "store";
-	  	push.autoSetupRemote = true;
-	  	commit.verbose = true;
-	  	help.autocorrect = "prompt";
-	  	diff.algorithm = "histogram";
-	  	merge.conflictstyle = "zdiff3";
-	  	branch.sort = "-committerdate";
-	  	# merge.tool = "meld";
-  	};
+    enable = true;
+
+    settings = {
+      init.defaultBranch = "master";
+      credential.helper = "store";
+      push.autoSetupRemote = true;
+      commit.verbose = true;
+      help.autocorrect = "prompt";
+      diff.algorithm = "histogram";
+      merge.conflictstyle = "zdiff3";
+      branch.sort = "-committerdate";
+
+      user = {
+        name = "bLaCkwEw";
+        email = "35146970+bLaCkwEw@users.noreply.github.com";
+      };
+    };
   };
 }
