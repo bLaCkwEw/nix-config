9d561197004b7e8e6b2fdeb722da110ea7edbe93
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2026-03-31 18:17:23 +0300

    add notification & multimediaKeys support to MPD

--- modules/music/default.nix
+++ modules/music/default.nix
@@ -1,17 +1,23 @@
-{ ... }:
-
-{
-  # Music daemon
-  services.mpd = {
-    enable = true;
-    musicDirectory = /home/blackwew/Music;
-    network.startWhenNeeded = true;
-    extraConfig = ''
-      audio_output {
-        type "pipewire"
-        name "Pipewire_OUT"
-      }
-    '';
+{...}: {
+  services = {
+    # Music daemon
+    mpd = {
+      enable = true;
+      musicDirectory = /home/blackwew/Music;
+      network.startWhenNeeded = true;
+      extraConfig = ''
+        audio_output {
+          type "pipewire"
+          name "Pipewire_OUT"
+        }
+      '';
+    };
+    # Media keys & notification support for MPD
+    mpdris2 = {
+      enable = true;
+      notifications = true;
+      multimediaKeys = true;
+    };
   };
   # Music player
   programs.rmpc = {
